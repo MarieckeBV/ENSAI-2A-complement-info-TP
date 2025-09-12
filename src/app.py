542cf8f75f1f8f3a59f3eaccaacf886df413e128
marieckebv <mariecke.baudru-vandenberghe@eleve.ensai.fr>
2025-09-12 14:19:58 +0000

    API

--- src/app.py
+++ src/app.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI, HTTPException
+from django.http import JsonResponse
 from pydantic import BaseModel
 from typing import Dict
-
+from starlette import status
+import uvicorn
 app = FastAPI()
 
 
@@ -22,11 +24,12 @@ class Personnage(BaseModel):
 
 
 # Create a dictionary to store characters
-characters_db: Dict[int, Personnage] = {}
+characters_db = dict()
 characters_db[1] = Personnage(nom="Anne", age=33)
 characters_db[2] = Personnage(nom="Michel", age=20)
 character_id = 3  # Initial character ID
 
+print(list(characters_db.values()))
 
 # Add a character
 @app.post("/character/")
@@ -36,9 +39,24 @@ def create_character(character: Personnage):
     character_id += 1
     return character
 
+# Get characters
+@app.get("/character")
+async def get_character():
+    return list(characters_db.values())
+
+#Modify character
+@app.put("/character/{id}")
+async def modify_character(id, new_name):
+    if id not in characters_db.keys():
+        return JsonResponse(status_code=status.HTTP_404_NOT_FOUND)
+    else:
+        characters_db[id].name = new_name
+
+
+
 
 # Run the FastAPI application
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=80)
+    uvicorn.run(app, host="0.0.0.0", port=9876)
